6324148fa10af43ff3d9251feb2f2dd48334a624
AccessiT3ch <accessiT3ch@gmail.com>
2022-04-22 14:35:25 -0700

    Adding Grid to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <main className="App">
+      <Container fluid>
+        <Row className="splash-row">
+          <Col>
+            <h2>Accessibility for Design Technologists</h2>
+            {/* todo: add social buttons */}
+          </Col>
+        </Row>
+      </Container>
+    </main>
   );
 }
 
