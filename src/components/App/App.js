a74c017bccf5d054008f4e89ce70ab991aaf4554
AccessiT3ch <accessiT3ch@gmail.com>
2022-04-22 14:35:36 -0700

    Init Header element

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Container fluid>
         <Row className="splash-row">
           <Col>
+            <Header />
             <h2>Accessibility for Design Technologists</h2>
             {/* todo: add social buttons */}
           </Col>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="main-header">
+      {/* todo: add accessibility options widget */}
+      <h1>AccessiTech LLC</h1>
+    </header>
+  )
+}
+
+export default Header;
