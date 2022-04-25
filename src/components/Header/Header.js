4638c7ed70b7b3a0e6cc52097d54ef7d0fa6fb15
AccessiT3ch <accessiT3ch@gmail.com>
2022-04-25 13:32:15 -0700

    Added splash logo to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
+  const headerStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}'/assets/images/TypeLogo_White_HC.svg')`
+  };
   return (
     <header className="main-header">
       {/* todo: add accessibility options widget */}
-      <h1>AccessiTech LLC</h1>
+      <h1 style={headerStyle}>AccessiTech LLC</h1>
     </header>
   )
 }
