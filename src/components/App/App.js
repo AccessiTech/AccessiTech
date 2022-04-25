b42c80176e89e145ee9911aa0bafab2b01047b58
AccessiT3ch <accessiT3ch@gmail.com>
2022-04-25 15:52:13 -0700

    Add Purpose Section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,6 +12,8 @@ function App() {
     backgroundImage: `url(${process.env.PUBLIC_URL}'/assets/images/splashBG.jpg')`,
   };
 
+  const purposePicSrc = `${process.env.PUBLIC_URL}/assets/images/hands.png`;
+
   return (
     <main className="App">
       <Container fluid>
@@ -29,6 +31,18 @@ function App() {
           <Col xs={12} md={{span: 8, offset: 2}}>
             <p>AccessiTech LLC is a social enterprise dedicated to removing barriers for disabled design technologists by producing and sharing relevant industry expertise and personal / professional exploration via content across multiple social, hosting, and streaming platforms.</p>
             <p>As a social enterprise, AccessiTech LLC fosters positive social change by promoting and collaborating on emergent technologies aimed at increasing accessibility, inclusion, and equity for all.</p>
+          </Col>
+        </Row>
+        <Row className="purpose-row">
+          <Col className="purpose-text" xs={12} md={{span: 4, offset: 2}}>
+            <h3>Purpose</h3>
+            <p>AccessiTech believes access to technology is a human right, particularly when such technology promotes, protects, and/or facilitates other human rights.</p>
+            <p>Producing and publishing content on accessibility in technology, AccessiTech LLC provides clear and concise information to help disabled peoples more readily utilize the accessibility options available in their lives in order to better enable them to pursue their passions in a digital world.</p>
+            <p>Through the proliferation, decentralization, and democratization of technology accessibility information, AccessiTech LLC seeks to lower the barriers to entry for disabled peoples throughout design and development communities abroad.</p>
+            <p>Collaborating and promoting design and technology for social change, AccessiTech LLC aims to help create an increasingly accessible world for all.</p>
+          </Col>
+          <Col className="purpose-image" xs={12} md={{span: 5}}>
+            <img src={purposePicSrc} className="purpose-picture" />
           </Col>
         </Row>
       </Container>
