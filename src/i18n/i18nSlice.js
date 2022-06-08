3fca31e2fd901e1c97c6b160d2053daffc385638
AccessiT3ch <accessiT3ch@gmail.com>
2022-06-08 15:04:48 -0700

    Add selectors for i18nSlice

--- src/i18n/i18nSlice.js
+++ src/i18n/i18nSlice.js
@@ -19,3 +19,25 @@ export const i18nSlice = createSlice({
 
 export default i18nSlice.reducer;
 
+// Selectors
+
+/**
+ * Get Current Language
+ * @returns (string|null) language key for current language
+ */
+export const getCurrentLang = () => {
+  const slice = useSelector((state) => state[i18nSliceName]);
+  return slice && slice.lang || null;
+}
+
+/**
+ * Get Translation
+ * @param stringKey (string) unique identifier for selecting translated string
+ * @param lang (optional string) lang code
+ * @returns (string|null) translated string or null if lang
+ */
+export const getT = (stringKey, lang = 'en') => {
+  const slice = useSelector((state) => state[i18nSliceName]);
+  const displayStrings = slice.translations && slice.translations[lang];
+  return displayStrings && displayStrings[stringKey] && displayStrings[stringKey].message || null;
+};
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,4 +1,11 @@
 import * as i18n from "./i18nSlice";
 
+// Redux Slice
 export const i18nSlice = i18n.default;
+
+// Redux Selectors
+export const getCurrentLang = i18n.getCurrentLang;
+export const getT = i18n.getT;
+
+// i18nSlice module
 export default i18n;
