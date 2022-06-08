417581ae6a103d120e9ad0c36447d761e03479ce
AccessiT3ch <accessiT3ch@gmail.com>
2022-06-08 15:05:32 -0700

    Change content strings to magic strings

--- src/settings/strings.js
+++ src/settings/strings.js
@@ -4,28 +4,29 @@ import { SPLASH_BG_URL } from "./settings";
 export const SITE_TITLE = 'AccessiTech';
 export const ACCESSITECH = 'AccessiTech';
 export const COMPANY_TITLE = 'AccessiTech LLC'
-export const TAGLINE = 'Accessibility for Design Technologists';
-export const LINKEDIN_LABEL = 'AccessiTech on LinkedIn';
-export const TWITTER_LABEL = 'AccessiTech on Twitter';
-export const YOUTUBE_LABEL = 'AccessiTech on YouTube';
-export const GMAIL_LABEL = 'AccessiTech on Gmail';
-export const ACCESSITECH_LOGO_LABEL = 'AccessiTech Logo';
-export const REDDIT_LABEL = 'AccessiTech on Reddit';
-export const GITHUB_LABEL = 'AccessiTech on GitHub';
-export const GITHUB_ISSUES = 'GitHub Issues';
+
+export const TAGLINE = 'TAGLINE';
+export const LINKEDIN_LABEL = 'LINKEDIN_LABEL';
+export const TWITTER_LABEL = 'TWITTER_LABELr';
+export const YOUTUBE_LABEL = 'YOUTUBE_LABEL';
+export const GMAIL_LABEL = 'GMAIL_LABEL';
+export const ACCESSITECH_LOGO_LABEL = 'ACCESSITECH_LOGO_LABEL';
+export const REDDIT_LABEL = 'REDDIT_LABEL';
+export const GITHUB_LABEL = 'GITHUB_LABEL';
+export const GITHUB_ISSUES = 'GITHUB_ISSUES';
 
 
 // Content Strings
-export const VISION_P1 = 'AccessiTech LLC is a social enterprise dedicated to removing barriers for disabled design technologists by producing and sharing relevant industry expertise and personal / professional exploration via content across multiple social, hosting, and streaming platforms.';
-export const VISION_P2 = 'As a social enterprise, AccessiTech LLC fosters positive social change by promoting and collaborating on emergent technologies aimed at increasing accessibility, inclusion, and equity for all.';
-export const PURPOSE_HEADER = 'Purpose';
-export const PURPOSE_P1 = 'AccessiTech believes access to technology is a human right, particularly when such technology promotes, protects, and/or facilitates other human rights.';
-export const PURPOSE_P2 = 'Producing and publishing content on accessibility in technology, AccessiTech LLC provides clear and concise information to help disabled peoples more readily utilize the accessibility options available in their lives in order to better enable them to pursue their passions in a digital world.';
-export const PURPOSE_P3 = 'Through the proliferation, decentralization, and democratization of technology accessibility information, AccessiTech LLC seeks to lower the barriers to entry for disabled peoples throughout design and development communities abroad.';
-export const PURPOSE_P4 = 'Collaborating and promoting design and technology for social change, AccessiTech LLC aims to help create an increasingly accessible world for all.';
-export const REACH_OUT_HEADER = 'Reach Out!';
-export const ISSUES_P1 = 'Report bugs, request features, and start collaborating via';
-export const COPYRIGHT_P1 = 'Copyright © 2021 AccessiTech LLC. All rights reserved.';
+export const VISION_P1 = 'VISION_P1';
+export const VISION_P2 = 'VISION_P2';
+export const PURPOSE_HEADER = 'PURPOSE_HEADER';
+export const PURPOSE_P1 = 'PURPOSE_P1';
+export const PURPOSE_P2 = 'PURPOSE_P2';
+export const PURPOSE_P3 = 'PURPOSE_P2';
+export const PURPOSE_P4 = 'PURPOSE_P2';
+export const REACH_OUT_HEADER = 'REACH_OUT_HEADER';
+export const ISSUES_P1 = 'ISSUES_P1';
+export const COPYRIGHT_P1 = 'COPYRIGHT_P1';
 
 // Magical Strings
 export const WHITE = 'white';
