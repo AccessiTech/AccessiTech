4d16212957d5ddd7a2bfac9a41a3f6aec57cbd02
AccessiT3ch <accessiT3ch@gmail.com>
2022-06-27 15:59:19 -0700

    Use Params as fallback to currentLang

--- src/components/A11Y/A11Y.js
+++ src/components/A11Y/A11Y.js
@@ -1,6 +1,6 @@
 import React  from 'react';
 import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getCurrentLang, getLanguageKeys, setLang } from '../../i18n';
 import { ROOT } from '../../settings/strings';
 import store from '../../store/store';
@@ -11,10 +11,14 @@ const namespace = 'a11y/'
 function A11Y() {
   let navigate = useNavigate();
   const languageKeys = getLanguageKeys();
+  const { lang } = useParams();
+  const currentLang = getCurrentLang() || lang;
+
   return (languageKeys && languageKeys.length) ? (
     <div className="a11y-container">
       <Form.Select
         className="language-select"
+        defaultValue={currentLang}
         onChange={(e) => {
           e.preventDefault();
           store.dispatch(setLang(e.target.value));
@@ -25,7 +29,6 @@ function A11Y() {
           <option
             className="language-select-option"
             key={`${namespace}/${i}`}
-            selected={lang === getCurrentLang()}
             value={lang}
           >{lang}</option>
         )}
