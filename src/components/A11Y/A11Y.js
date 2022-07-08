160bbecfbb819ee5673119628822e991081ffc87
AccessiT3ch <accessiT3ch@gmail.com>
2022-07-08 14:02:56 -0700

    Add flags emojis to selector

--- src/components/A11Y/A11Y.js
+++ src/components/A11Y/A11Y.js
@@ -2,6 +2,8 @@ import React  from 'react';
 import Form from 'react-bootstrap/Form';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getCurrentLang, getLanguageKeys, langKeyDisplayStrings, setLang } from '../../i18n';
+import { translationFlags } from '../../i18n/translations';
+import { EN } from '../../settings/strings';
 import store from '../../store/store';
 import './a11y.scss';
 
@@ -18,6 +20,9 @@ function A11Y() {
       className="a11y-container"
       aria-label="Accessibility Options"
     >
+      <span role="img" aria-hidden="true" className="language-flag">
+        {translationFlags[lang || EN]}
+      </span>
       <Form.Select
         aria-label="Lanage Selection"
         className="language-select"
@@ -33,7 +38,9 @@ function A11Y() {
             className="language-select-option"
             key={`${namespace}/${i}`}
             value={lang}
-          >{langKeyDisplayStrings[lang]}</option>
+          >
+            {langKeyDisplayStrings[lang]}
+          </option>
         )}
       </Form.Select>
     </div>
--- src/i18n/translations/index.js
+++ src/i18n/translations/index.js
@@ -1,15 +1,23 @@
 import en from './en.json';
 import es from './es.json';
 import fr from './fr.json';
+
 const translations = {
   en,
   es,
   fr,
 };
+
 export default translations;
 
 export const displayStrings = {
   en: 'En',
   es: 'Es',
   fr: 'Fr',
+};
+
+export const translationFlags = {
+  en: '🇺🇸',
+  es: '🇪🇸',
+  fr: '🇫🇷',
 };
