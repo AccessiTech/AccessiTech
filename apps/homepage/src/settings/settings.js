dab9fe4d8c82580cafb0339389ebfdb3ac86667c
AccessiT3ch <accessiT3ch@gmail.com>
2022-08-16 11:48:27 -0700

    homepage: use smaller purpose img

--- apps/homepage/src/settings/settings.js
+++ apps/homepage/src/settings/settings.js
@@ -1,4 +1,4 @@
 import { IMAGES_URL } from "./env";
 
 export const SPLASH_BG_URL = `${IMAGES_URL}/splashBG.jpg`;
-export const PURPOSE_PIC_URL = `${IMAGES_URL}/hands.png`;
+export const PURPOSE_PIC_URL = `${IMAGES_URL}/hands.jpg`;
