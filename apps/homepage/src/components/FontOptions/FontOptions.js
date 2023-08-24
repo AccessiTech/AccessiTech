0bc4c8b840f8cb63b13ed7e65106dcbdd3a58614
AccessiT3ch <accessiT3ch@gmail.com>
2023-08-24 16:34:06 -0700

    Homepage: Add translations for font-options

--- apps/homepage/src/components/FontOptions/FontOptions.js
+++ apps/homepage/src/components/FontOptions/FontOptions.js
@@ -1,43 +1,48 @@
-import React  from 'react';
+import React from "react";
 import {
   debounce,
   toggleFontOptions,
   useFontFamily,
   useFontSize,
   useIsOpen,
-} from './reducer';
-import store from '../../store/store';
-import { onFontFamilyChange, onFontOptionsToggle, onFontSizeChange } from './helpers';
+} from "./reducer";
+import store from "../../store/store";
+import {
+  onFontFamilyChange,
+  onFontOptionsToggle,
+  onFontSizeChange,
+} from "./helpers";
+import { useT } from "@accessitech/i18n-redux-toolkit";
 
-export const namespace = 'fontOptions/';
+export const namespace = "fontOptions/";
 
 // Display Strings!
-export const FONT_OPTIONS_TOGGLE_LABEL = 'Toggle Font Options Menu';
-export const FONT_OPTIONS_TITLE = 'Font Options';
-export const FONT_OPTIONS_DESCRIPTION = 'Change the font size and family of the page';
-export const FONT_SIZE_LABEL = 'Font Size: ';
-export const FONT_OPTIONS_FAMILY_LABEL = 'Font Family: ';
-export const FONT_OPTIONS_SIZE_INPUT_LABEL = 'Font Size Range Input';
-export const FONT_FAMILY_SELECT_LABEL = 'Font Family Select';
-export const SERIF_DISPLAY = 'Serif';
-export const SANS_SERIF_DISPLAY = 'Sans Serif';
-export const MONOSPACE_DISPLAY = 'Monospace';
+export const FONT_OPTIONS_TOGGLE_LABEL = "FONT_OPTIONS_TOGGLE_LABEL";
+export const FONT_OPTIONS_TITLE = "FONT_OPTIONS_TITLE";
+export const FONT_OPTIONS_DESCRIPTION = "FONT_OPTIONS_DESCRIPTION";
+export const FONT_SIZE_LABEL = "FONT_SIZE_LABEL";
+export const FONT_OPTIONS_FAMILY_LABEL = "FONT_OPTIONS_FAMILY_LABEL";
+export const FONT_OPTIONS_SIZE_INPUT_LABEL = "FONT_OPTIONS_SIZE_INPUT_LABEL";
+export const FONT_FAMILY_SELECT_LABEL = "FONT_FAMILY_SELECT_LABEL";
+export const SERIF_DISPLAY = "SERIF_DISPLAY";
+export const SANS_SERIF_DISPLAY = "SANS_SERIF_DISPLAY";
+export const MONOSPACE_DISPLAY = "MONOSPACE_DISPLAY";
 
 // Magic Strings!!
-export const SERIF = 'serif';
-export const SANS_SERIF = 'sans-serif';
-export const MONOSPACE = 'monospace';
-export const ESCAPE = 'Escape';
+export const SERIF = "serif";
+export const SANS_SERIF = "sans-serif";
+export const MONOSPACE = "monospace";
+export const ESCAPE = "Escape";
 
 /** Font Options
- * @returns {object} font options component  
+ * @returns {object} font options component
  **/
 function FontOptions(props) {
   const isOpen = useIsOpen();
   const fontSize = Number(useFontSize() || 1).toFixed(1);
   const fontFamily = useFontFamily();
   const { onClose } = props || {};
-  
+
   const onEscapeKey = (e) => {
     if (e.key === ESCAPE) {
       if (isOpen) {
@@ -46,69 +51,57 @@ function FontOptions(props) {
         onClose(e);
       }
     }
-  }
+  };
 
-  return (
-    <div className="font-options-container">
-      <button
-        className={`${isOpen ? "isOpen " : ""}font-options-toggle`}
-        onClick={onFontOptionsToggle}
-        aria-label={FONT_OPTIONS_TOGGLE_LABEL}
-        onKeyDown={onEscapeKey}
-      >
-        <i className="fa-solid fa-font" />
-      </button>
-      {isOpen && (
-        <form id={`${namespace}form`} className="font-options">
-          <fieldset>
-            <legend>{FONT_OPTIONS_TITLE}</legend>
+  const formElement = (
+    <form id={`${namespace}form`} className="font-options">
+      <fieldset>
+        <legend>{useT(FONT_OPTIONS_TITLE)}</legend>
 
-            <div className="font-options__row">
-              <div className="flex-row flex-space-between">
-                <label htmlFor="font-options__font-size">
-                  {FONT_SIZE_LABEL}
-                </label>
-                <span className="font-options__font-size-value">
-                  {fontSize}x
-                </span>
-              </div>
+        <div className="font-options__row">
+          <div className="flex-row flex-space-between">
+            <label htmlFor="font-options__font-size">
+              {useT(FONT_SIZE_LABEL)}:
+            </label>
+            <span className="font-options__font-size-value">{fontSize}x</span>
+          </div>
 
-              <input
-                type="range"
-                id="font-options__font-size"
-                name="font-options__font-size"
-                aria-label={FONT_OPTIONS_SIZE_INPUT_LABEL}
-                min="0.5"
-                max="5"
-                step={0.1}
-                defaultValue={fontSize}
-                onChange={(e) => debounce(onFontSizeChange(e, fontSize), 250)}
-                onKeyDown={onEscapeKey}
-              />
-            </div>
+          <input
+            type="range"
+            id="font-options__font-size"
+            name="font-options__font-size"
+            aria-label={useT(FONT_OPTIONS_SIZE_INPUT_LABEL)}
+            min="0.5"
+            max="5"
+            step={0.1}
+            defaultValue={fontSize}
+            onChange={(e) => debounce(onFontSizeChange(e, fontSize), 250)}
+            onKeyDown={onEscapeKey}
+          />
+        </div>
 
-            <div className="font-options__row">
-              <div className="flex-row flex-space-between">
-                <label htmlFor="font-options__font-family">
-                  {FONT_OPTIONS_FAMILY_LABEL}
-                </label>
+        <div className="font-options__row">
+          <div className="flex-row flex-space-between">
+            <label htmlFor="font-options__font-family">
+              {useT(FONT_OPTIONS_FAMILY_LABEL)}:
+            </label>
 
-                <select
-                  id="font-options__font-family"
-                  name="font-options__font-family"
-                  aria-label={FONT_FAMILY_SELECT_LABEL}
-                  defaultValue={fontFamily}
-                  onChange={onFontFamilyChange}
-                  onKeyDown={onEscapeKey}
-                >
-                  <option value={SANS_SERIF}>{SANS_SERIF_DISPLAY}</option>
-                  <option value={SERIF}>{SERIF_DISPLAY}</option>
-                  <option value={MONOSPACE}>{MONOSPACE_DISPLAY}</option>
-                </select>
-              </div>
-            </div>
+            <select
+              id="font-options__font-family"
+              name="font-options__font-family"
+              aria-label={useT(FONT_FAMILY_SELECT_LABEL)}
+              defaultValue={fontFamily}
+              onChange={onFontFamilyChange}
+              onKeyDown={onEscapeKey}
+            >
+              <option value={SANS_SERIF}>{useT(SANS_SERIF_DISPLAY)}</option>
+              <option value={SERIF}>{useT(SERIF_DISPLAY)}</option>
+              <option value={MONOSPACE}>{useT(MONOSPACE_DISPLAY)}</option>
+            </select>
+          </div>
+        </div>
 
-            {/* <div className="font-options__row">
+        {/* <div className="font-options__row">
               <label htmlFor="font-options__font-weight">Font Color</label>
               <input type="color" id="font-options__font-weight" name="font-options__font-weight" />
             </div>
@@ -124,9 +117,21 @@ function FontOptions(props) {
               <label htmlFor="font-options__bionic">Bionic</label>
               <input type="checkbox" id="font-options__bionic" name="font-options__bionic" />
             </div> */}
-          </fieldset>
-        </form>
-      )}
+      </fieldset>
+    </form>
+  );
+
+  return (
+    <div className="font-options-container">
+      <button
+        className={`${isOpen ? "isOpen " : ""}font-options-toggle`}
+        onClick={onFontOptionsToggle}
+        aria-label={useT(FONT_OPTIONS_TOGGLE_LABEL)}
+        onKeyDown={onEscapeKey}
+      >
+        <i className="fa-solid fa-font" />
+      </button>
+      {isOpen && formElement}
     </div>
   );
 }
