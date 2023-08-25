0fd384aa4e2cfdb887195f47f5c0bf4ec6031e8b
AccessiT3ch <accessiT3ch@gmail.com>
2023-08-25 15:09:10 -0700

    Homepage: pre PR cleanup

--- apps/homepage/src/App/App.js
+++ apps/homepage/src/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../pages/Home/Home';
-import About from '../pages/About/About';
 import '../sass/global.scss';
 
 const App = () => {
@@ -10,8 +9,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:lang" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/:lang/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
