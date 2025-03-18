0a485457c557b03edd88dfc33567a8851f520e21
AccessiT3ch <accessiT3ch@gmail.com>
2025-03-18 15:18:54 -0700

    Fix env reference in env.ts

--- src/settings/env.ts
+++ src/settings/env.ts
@@ -1,3 +1,4 @@
-export const PUBLIC_URL = process.env.PUBLIC_URL;
+export const env = await import.meta.env;
+export const PUBLIC_URL = env.VITE_PUBLIC_URL;
 export const ASSETS_URL = `${PUBLIC_URL}/assets`;
 export const IMAGES_URL = `${ASSETS_URL}/images`;
