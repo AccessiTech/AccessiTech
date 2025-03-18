058c05d610e8547d31372e299063bded521cef6c
AccessiT3ch <accessiT3ch@gmail.com>
2025-03-18 15:32:11 -0700

    Add empty string fallback for PUBLIC_URL

--- src/settings/env.ts
+++ src/settings/env.ts
@@ -1,4 +1,4 @@
 export const env = await import.meta.env;
-export const PUBLIC_URL = env.VITE_PUBLIC_URL;
+export const PUBLIC_URL = env.VITE_PUBLIC_URL || '';
 export const ASSETS_URL = `${PUBLIC_URL}/assets`;
 export const IMAGES_URL = `${ASSETS_URL}/images`;
