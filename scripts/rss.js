5a6258980c9f2de89bf9b7ec0be40aed514cd60c
AccessiT3ch <accessiT3ch@gmail.com>
2025-03-20 15:57:48 -0700

    Get metadata from files for RSS script

--- scripts/rss.js
+++ scripts/rss.js
@@ -3,6 +3,19 @@
 import fs from 'fs';
 import path from 'path';
 
+const getMetaData = (text) => {
+  const metaData = {};
+  const lines = text.split("\n");
+  lines.forEach((line) => {
+    const key = line.split(":")[0]?.replace("<!--", "").trim();
+    const value = line.split(":")[1]?.replace("-->", "").trim();
+    if (key && value) {
+      metaData[key] = value;
+    }
+  });
+  return metaData;
+};
+
 const blogDir = path.join(process.cwd(), 'public/blog');
 const blogFiles = fs.readdirSync(blogDir);
 
@@ -16,14 +29,15 @@ const rss = `<?xml version="1.0" encoding="UTF-8" ?>
       const filePath = path.join(blogDir, file);
       const fileContent =
         fs.readFileSync(filePath, { encoding: 'utf-8' });
-      const title = fileContent.split('\n')[0].replace('# ', '');
-      const link = `https://accessi.tech/blog/${file}`.replace('.md', '');
-      const pubDate = new Date(fs.statSync(filePath).mtime).toUTCString();
+        const link = `https://accessi.tech/blog/${file}`.replace('.md', '');
+      const fileMetaData = getMetaData(fileContent);
+      const { title, date, description } = fileMetaData;
       return `
         <item>
           <title>${title}</title>
           <link>${link}</link>
-          <pubDate>${pubDate}</pubDate>
+          <pubDate>${date}</pubDate>
+          <description>${description}</description>
         </item>
       `;
     }
