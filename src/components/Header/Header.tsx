0f2149c62907fd3ed9c578e5bfd13702b40efa17
AccessiT3ch <accessiT3ch@gmail.com>
2025-03-20 12:37:05 -0700

    Start styling for non-home headers

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,28 @@
+import { Link, useLocation } from 'react-router-dom';
 import { SKIP_TO_MAIN_CONTENT } from '../../pages/Home/Home';
 import { IMAGES_URL } from '../../settings/env';
 import { COMPANY_TITLE, ROOT } from '../../settings/strings';
 import A11Y from '../A11Y/A11Y';
 import './Header.scss';
+import { useEffect, useState } from 'react';
+import { getPageFromPath } from '../../settings/utils';
 
 export const HEADER = 'Header';
 
 function Header() {
+  const location = useLocation();
+  const { pathname } = location;
+  const [page, setPage] = useState(getPageFromPath(pathname));
+
+  useEffect(() => {
+    setPage(getPageFromPath(pathname));
+  }, [pathname]);
+
   const headerStyle = {
     backgroundImage: `url('${IMAGES_URL}/TypeLogo_White_HC.svg')`,
   };
   return (
-    <header className="main-header" aria-label={HEADER}>
+    <header className={`main-header ${page}`} aria-label={HEADER}>
       <a className="skip-link" href="#main">{(SKIP_TO_MAIN_CONTENT)}</a>
       <A11Y />
       <a
