d443e81d90f070cab72acfcfdc23f350040d03fe
AccessiT3ch <accessiT3ch@gmail.com>
2025-03-26 13:33:00 -0700

    Use promise.all in gen-static for multi-thread gen

--- scripts/gen-static.js
+++ scripts/gen-static.js
@@ -4,11 +4,13 @@ import path from "node:path";
 import fs from "node:fs";
 
 import { XMLParser } from "fast-xml-parser";
+import { getMetaData } from "./rss.js";
 
 export const CONFIG = {
-  urlSrc: path.resolve(process.cwd(), "public/rss.xml"),
-  dest: path.resolve(process.cwd(), "docs"),
+  urlSrc: "public/rss.xml",
+  dest: "docs",
   staticPaths: ["/", "/blog"],
+  staticMetaData: ["src/App/meta.ts", "src/pages/Blog/meta.ts"],
   pathsBuilder: (items) =>
     items.map((item) => {
       const { link } = item;
@@ -21,13 +23,13 @@ export const CONFIG = {
     server: { middlewareMode: true },
     appType: "custom",
   },
-  ssrEntry: path.resolve(process.cwd(), "src/server.tsx"),
+  ssrEntry: "src/server.tsx",
   // spread the process args onto the config
   ...process.argv,
 };
 
 export async function genUrls(config) {
-  const RSS = fs.readFileSync(config.urlSrc, "utf-8");
+  const RSS = fs.readFileSync(path.resolve(process.cwd(), config.urlSrc), "utf-8");
   const parser = new XMLParser();
   const rssOjb = parser.parse(RSS);
   const items = rssOjb.rss.channel.item?.length
@@ -44,28 +46,28 @@ export async function genUrls(config) {
 export async function genStatic({ config, urls }) {
   const vite = await createServer(config.viteServer).catch((err) => {
     console.error(err);
-    process.exit(1);
+    throw new Error(err);
   });
   console.log("Vite server created");
 
-  for (const url of urls) {
+  const vitePromises = urls.map(async (url, index) => {
     console.log("Vite module loading for ", url);
     const { render } = await vite
-      .ssrLoadModule(config.ssrEntry)
+      .ssrLoadModule(path.resolve(process.cwd(), config.ssrEntry))
       .catch((err) => {
         console.error(err);
-        process.exit(1);
+        throw new Error(err);
       });
-
-    const toBuildPath = (pathPart) => path.join(config.dest, pathPart);
+    const toBuildPath = (pathPart) => path.join(path.resolve(process.cwd(), config.dest), pathPart);
     const indexHtmlContent = fs
       .readFileSync(toBuildPath("index.html"))
       .toString();
 
     const urlHtmlMarkup = await render(url);
     if (!urlHtmlMarkup) {
-      console.error(`No content rendered for ${url}`);
-      process.exit(1);
+      const errorStr = `No content rendered for ${url}`;
+      console.error(errorStr);
+      throw new Error(errorStr);
     }
 
     // update the index.html with the rendered markup
@@ -73,11 +75,35 @@ export async function genStatic({ config, urls }) {
       '<div id="root"></div>',
       `<div id="root">${urlHtmlMarkup}</div>`
     );
+
+    // get the page metadata
+    let metadata;
+    const isStatic = typeof config.staticMetaData[index] !== "undefined";
+    if (isStatic) {
+      const metadataPath = path.resolve(
+        process.cwd(),
+        config.staticMetaData[index]
+      );
+      metadata = (await import(metadataPath)).default;
+    } else {
+      const fileContent = fs.readFileSync(
+        path.resolve(
+          process.cwd(),
+          "public/data/blog",
+          `${url.split("/").pop()}.md`
+        ),
+        { encoding: "utf-8" }
+      );
+      metadata = getMetaData(fileContent);
+    }
+
+    console.log("METADATA", metadata);
+
     // parse the index.html head into an array of strings
     const headString = indexHtmlContent.match(/<head>[\s\S]*<\/head>/)[0];
     // split head string by each tag
     const head = headString.split(/(?=<\/?[^>]+>)/);
-    console.log("HEAD", head);
+    // console.log("HEAD", head);
     // update the index.html head with new metadata tags
     // const urlHtmlContent = indexHtmlContent.replace(
     //   '<head>',
@@ -87,17 +113,22 @@ export async function genStatic({ config, urls }) {
     }
     fs.writeFileSync(toBuildPath(url + ".html"), urlHtmlContent);
     console.log(`Static page generated for ${url}`);
-  }
-  // shutdown vite
-  await vite.close();
+  });
+
+  Promise.all(vitePromises)
+    .then(() => {
+      console.log("All static pages generated");
+      return vite.close();
+    })
+    .catch((e) => {
+      console.error("Error generating static pages: ", e);
+      throw new Error(e);
+    });
 }
 
 ((config) =>
   genUrls(config)
     .then(genStatic)
-    .then(() => {
-      console.log("All static pages generated");
-    })
     .catch((err) => {
       console.error("Error generating static pages: ", err);
       process.exit(1);
