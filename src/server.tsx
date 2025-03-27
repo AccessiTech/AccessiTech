738adf80162c00efef9c9d135693fb1bfddea16b
AccessiT3ch <accessiT3ch@gmail.com>
2025-03-26 18:44:09 -0700

    Add renderMetadata functionality to server

--- src/server.tsx
+++ src/server.tsx
@@ -1,21 +1,25 @@
 // import ReactDOMServer from 'react-dom/server'
 import { Provider } from 'react-redux'
 import App from './App/AppServer'
+import Metadata from './components/Metadata/Metadata'
 import './scss/index.scss'
 import { store } from './store/store'
+import { MetaDataProps } from './settings/getMetaData'
 
 console.log('Hello from server.tsx')
 
 export const render = async (path:string) => {
-  // console.log('Rendering path:', path)
   const ReactDOMServer = (await import('react-dom/server')).default;
   const component = (<Provider store={store}>
     <App path={path} />
   </Provider>);
   const staticMarkup = ReactDOMServer.renderToStaticMarkup(component);
-  // const staticString = ReactDOMServer.renderToString(<Root path={path} />);
-  // console.log('staticMarkup:', staticMarkup)
-  // console.log('staticString:', staticString)
   return staticMarkup;
 }
 
+export const renderMetadata = async (data:MetaDataProps) => {
+  const ReactDOMServer = (await import('react-dom/server')).default;
+  const component = (<Metadata {...data} />);
+  const staticMarkup = ReactDOMServer.renderToStaticMarkup(component);
+  return staticMarkup;
+}
