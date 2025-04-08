17805939c4e298b9c9b66d84f1a9a3b8cea4042e
AccessiT3ch <accessiT3ch@gmail.com>
2025-04-08 12:40:50 -0700

    Move ssg configuration to ssg.config.ts

--- scripts/gen-static.ts
+++ scripts/gen-static.ts
@@ -1,37 +1,5 @@
-import reactPlugin from "@vitejs/plugin-react-swc";
-import { generate, CONFIG, ConfigProps } from "@accessitech/vite-ssg";
-
-const config: ConfigProps = {
-  ...CONFIG,
-  urlSrc: process.cwd() + "/public/rss.xml",
-  dest: process.cwd() + "/docs",
-  staticPaths: ["/", "/blog"],
-  staticMetaData: [
-    process.cwd() + "/src/App/meta.ts",
-    process.cwd() + "/src/pages/Blog/meta.ts",
-  ],
-  productionUrlBase: "https://accessi.tech",
-  pathsBuilder: (items) =>
-    items.map((item) => {
-      const { link } = item;
-      const id = link.split("/").pop()?.replace(".md", "") || "";
-      return `/blog/${id}`;
-    }),
-  viteServer: {
-    ...CONFIG.viteServer,
-    plugins: [reactPlugin()],
-    root: process.cwd(),
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: "modern-compiler",
-          quietDeps: true,
-          additionalData: `@import "${process.cwd()}/src/scss/variables.scss";`,
-        },
-      },
-    },
-  },
-};
+import { generate } from "@accessitech/vite-ssg";
+import config from "../ssg.config";
 
 (async () => {
   console.log("Starting static site generation...");
