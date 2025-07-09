48a1fdf652777a9ebc2db118fe0c60bc457f9522
Conor <95449364+AccessiT3ch@users.noreply.github.com>
2025-07-09 11:51:10 -0700

    Check for env before checking PROD

--- src/settings/env.ts
+++ src/settings/env.ts
@@ -2,4 +2,4 @@ export const env = await import.meta.env;
 export const PUBLIC_URL = env?.VITE_PUBLIC_URL || '';
 export const ASSETS_URL = `${PUBLIC_URL}/assets`;
 export const IMAGES_URL = `${ASSETS_URL}/images`;
-export const SITE_HOST = env?.VITE_SITE_HOST || env.PROD ? 'accessi.tech' : 'localhost';
+export const SITE_HOST = env?.VITE_SITE_HOST || (env?.PROD ? 'accessi.tech' : 'localhost');
