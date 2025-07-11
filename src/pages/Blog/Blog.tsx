6544d0096ab4a949fed9a52b94821c5841b568ad
Conor <95449364+AccessiT3ch@users.noreply.github.com>
2025-07-11 09:56:30 -0700

    Always fetch blogEntries when pathname updates

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -64,7 +64,7 @@ export const Blog: BlogType = ({ hideDates, hideDescription, hideExcerpt }: Blog
 
   useEffect(() => {
     fetchBlogEntries({pathname});
-  }, []);
+  }, [pathname]);
 
   return (<>
     <Row className="header-row">
