4ef65bf87000921eb6e13c2ad74fcf1883895042
Conor <95449364+AccessiT3ch@users.noreply.github.com>
2025-07-21 19:52:28 -0700

    Improve Vitest SCSS mocking and add react-helmet-async
    Adds a custom Vite plugin and test setup to consistently mock SCSS files during tests, reducing import errors. Updates test scripts, refines setupTests.ts, and introduces react-helmet-async as a dependency.

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,18 +1,10 @@
 import { vi } from 'vitest';
 
-import '@testing-library/jest-dom';
-
-// Explicitly mock all SCSS files used by Header and its dependencies
-vi.mock('../components/Header/Header.scss', () => ({}));
+// Mock all .scss files
+vi.mock('../scss/index.scss', () => ({}));
 vi.mock('../components/SplashSocials/SplashSocials.scss', () => ({}));
 vi.mock('../components/SectionHeader/SectionHeader.scss', () => ({}));
-vi.mock('../components/Services/Services.scss', () => ({}));
-vi.mock('../scss/index.scss', () => ({}));
 vi.mock('react-bootstrap/scss/bootstrap.scss', () => ({}));
-vi.mock('bootstrap/scss/bootstrap.scss', () => ({}));
-vi.mock('../../scss/variables.scss', () => ({}));
-
-// Mock browser APIs
 
 // Mock browser APIs
 Object.defineProperty(window, 'matchMedia', {
